c8ce88b42fbfc7abaa47311c4a797efaed34b4ae
sophialee0416 <sophia.lee0416@gmail.com>
2018-10-05 12:14:00 -0500

    fixed bug, not sure if hacky so confirm if its the right approach

--- appAIAA/app/src/main/java/comsophialee0416aiaa/httpsgithub/appaiaa/RSSFeedFragment.kt
+++ appAIAA/app/src/main/java/comsophialee0416aiaa/httpsgithub/appaiaa/RSSFeedFragment.kt
@@ -8,6 +8,6 @@ import android.view.ViewGroup
 
 class RSSFeedFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_rssfeed)
+        return inflater!!.inflate(R.layout.fragment_rssfeed, null)
     }
 }
