dd54daf9a23f66580d8bdb935d171d513a7ab046
sophialee0416 <sophia.lee0416@gmail.com>
2018-10-05 11:08:53 -0500

    Added fragment XMLs, need to finish adding Fragment.kt for Calculator and Videos, some minor errors need to be addressed because build is failing

---
+++ appAIAA/app/src/main/java/comsophialee0416aiaa/httpsgithub/appaiaa/RSSFeedFragment.kt
@@ -0,0 +1,13 @@
+package comsophialee0416aiaa.httpsgithub.appaiaa
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class RSSFeedFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.fragment_rssfeed)
+    }
+}
