f3fe83c1c268bbd5a1656dc603fb6134a58f7a13
sophialee0416 <sophia.lee0416@gmail.com>
2018-10-08 18:20:31 -0500

    fragments work properly now

--- appAIAA/app/src/main/java/comsophialee0416aiaa/httpsgithub/appaiaa/RSSFeedFragment.kt
+++ appAIAA/app/src/main/java/comsophialee0416aiaa/httpsgithub/appaiaa/RSSFeedFragment.kt
@@ -1,13 +1,26 @@
 package comsophialee0416aiaa.httpsgithub.appaiaa
 
-import android.app.Fragment
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
+//1
 class RSSFeedFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return inflater!!.inflate(R.layout.fragment_rssfeed, null)
+
+    //2
+    companion object {
+
+        fun newInstance(): RSSFeedFragment {
+            return RSSFeedFragment()
+        }
     }
+
+    //3
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_rssfeed, container, false)
+    }
+
+
 }
