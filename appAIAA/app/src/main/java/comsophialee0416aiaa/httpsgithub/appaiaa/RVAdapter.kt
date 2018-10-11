9008a35b736273f6571ffbbf4854580a7517476f
sophialee0416 <sophia.lee0416@gmail.com>
2018-10-11 18:40:52 -0500

    Basic RecyclerView works, but cards align vertically not horizontally. Cards are hardcoded

---
+++ appAIAA/app/src/main/java/comsophialee0416aiaa/httpsgithub/appaiaa/RVAdapter.kt
@@ -0,0 +1,40 @@
+package comsophialee0416aiaa.httpsgithub.appaiaa
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+class MyAdapter(private val links:List<Card>): RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            MyViewHolder {
+
+        val itemLayoutView:View =  LayoutInflater.from(parent.context)
+                .inflate(R.layout.rssfeed_cards, null)
+
+        val vh:MyViewHolder = MyViewHolder(itemLayoutView)
+        return vh
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.cardTitle.text = links[position].title
+        holder.cardContent.text = links[position].content
+        //holder.itemUrl.text = links[position].url
+    }
+
+    override fun getItemCount(): Int {
+        return links?.size ?: 0
+    }
+
+    class MyViewHolder(itemLayoutView: View)
+        : RecyclerView.ViewHolder(itemLayoutView) {
+
+        val cardTitle = itemLayoutView.findViewById<TextView>(R.id.card_Title)
+        val cardContent = itemLayoutView.findViewById<TextView>(R.id.card_content)
+        //val itemUrl = itemLayoutView.findViewById<TextView>(R.id.item_url)
+
+    }
+}
