6fb6de871933e5de2d22fb3f2e80056402829b61
root <root@DESKTOP-QOC1HKH.localdomain>
2018-11-20 05:53:07 -0600

    operations work yisss

--- appAIAA/app/src/main/java/comsophialee0416aiaa/httpsgithub/appaiaa/CalculatorFragment.kt
+++ appAIAA/app/src/main/java/comsophialee0416aiaa/httpsgithub/appaiaa/CalculatorFragment.kt
@@ -2,17 +2,11 @@ package comsophialee0416aiaa.httpsgithub.appaiaa
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.system.Os.bind
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_calculator.*
-import java.text.DecimalFormat
-import java.text.DecimalFormatSymbols
-import java.text.ParseException
 
 
 class CalculatorFragment : Fragment() {
@@ -56,29 +50,59 @@ class CalculatorFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         calculate_button.setOnClickListener {
-            val input: Double = thrust_text.text.toString().toDouble()
-            var output: Double = massFlow(input)
-            flowkg_value.text = output.toString()
+            val thrust: Double = thrust_text.text.toString().toDouble()
+            val isp: Double = isp_text.text.toString().toDouble()
+            val power: Double = power_text.text.toString().toDouble()
+            val propellant: Double = propellant_text.text.toString().toDouble()
+            val mass: Double = mass_text.text.toString().toDouble()
+            val burnDays: Double = burndays_text.text.toString().toDouble()
+
+            val kg: Double = massFlowKg(thrust, isp, propellant)
+            val g: Double = massFlowg(kg)
+            val anode: Double = anodeEfficiency(thrust, kg, power)
+            val burnSec: Double = burnSeconds(burnDays)
+            val accelerate: Double = acceleration(thrust, mass)
+            val impulse: Double = approxImpulse(thrust, burnSec)
+            val massuse: Double = massUsed(kg, burnSec)
+
+            flowkg_value.text = kg.toString()
+            flowg_value.text = g.toString()
+            anode_value.text = anode.toString()
+            burnsec_value.text = burnSec.toString()
+            acceleration_value.text = accelerate.toString()
+            impulse_value.text = impulse.toString()
+            massused_value.text = massuse.toString()
         }
     }
 
-        fun massFlow(input: Double): Double {
+    private fun massFlowKg(thrust: Double, isp: Double, propellant: Double): Double {
+        return (thrust / (9.8 * isp) / propellant)
+    }
 
-//        var year: Int = input.toInt();
-//        var currentYear: Int = Calendar.getInstance().get(Calendar.YEAR);
-//
-//        var Result: String;
-//
-//        if(currentYear<year || year<1910) {
-//
-//            return "Enter Correct Year";
-//        }else {
-//
-//            var Age: Int = currentYear - year;
-//            Result = " Your are $Age year(s) old ";
-//        }
-        return input;
+    private fun massFlowg(kg: Double): Double {
+        return (kg * 1000 * 3600 * 24 * 30)
+    }
+
+    private fun anodeEfficiency(thrust: Double, kg: Double, power: Double): Double {
+        return (thrust * thrust) / (2 * kg * power)
+    }
+
+    private fun burnSeconds(burnDays: Double): Double {
+        return (3600 * 24 * burnDays)
+    }
+
+    private fun acceleration(thrust: Double, mass: Double): Double {
+        return (thrust / mass)
+    }
+
+    private fun approxImpulse(thrust: Double, burnSec: Double): Double {
+        return (thrust / (1.3 * burnSec))
+    }
+
+    private fun massUsed(kg: Double, burnSec: Double): Double {
+        return (kg * burnSec)
     }
 
 }
