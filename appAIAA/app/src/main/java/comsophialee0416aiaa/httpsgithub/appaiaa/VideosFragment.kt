43443ab64a74adfdc614d1224eca7952582851e8
root <root@DESKTOP-QOC1HKH.localdomain>
2018-12-13 00:05:29 -0600

    video works to what dr hara wanted but i need to work on it more :(

--- appAIAA/app/src/main/java/comsophialee0416aiaa/httpsgithub/appaiaa/VideosFragment.kt
+++ appAIAA/app/src/main/java/comsophialee0416aiaa/httpsgithub/appaiaa/VideosFragment.kt
@@ -1,29 +1,23 @@
 package comsophialee0416aiaa.httpsgithub.appaiaa
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.Toast
-import com.google.android.youtube.player.YouTubeInitializationResult
-import com.google.android.youtube.player.YouTubePlayer
-import com.google.android.youtube.player.YouTubePlayerView
-import com.google.android.youtube.player.YouTubeStandalonePlayer
 import kotlinx.android.synthetic.main.fragment_videos.*
 
 //1
 class VideosFragment : Fragment() {
 
-    lateinit var play: Button
-    lateinit var youtubePlayer: YouTubePlayerView
-    lateinit var youtubePlayerInit: YouTubePlayer.OnInitializedListener
+    lateinit var go: Button
+
 
     //2
     companion object {
-        val VIDEO_ID: String = "ckOJChcFP2g"
-        val YOUTUBE_API_KEY: String = "AIzaSyC1umsPU8msFm4U4ZBmxhqgY-7Q53VSFao"
 
         fun newInstance(): VideosFragment {
             return VideosFragment()
@@ -31,72 +25,20 @@ class VideosFragment : Fragment() {
     }
 
     //3
-
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        initUI()
-//    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        val view: View = inflater?.inflate(R.layout.fragment_videos, container, false)
-        this.play = view.findViewById(R.id.playButton) as Button
-        this.youtubePlayer = view.findViewById(R.id.youtubePlayerView) as YouTubePlayerView
-
-//        this.youtubePlayerInit = object : YouTubePlayer.OnInitializedListener {
-//            override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, youtubePlayer: YouTubePlayer, p2: Boolean) {
-//                youtubePlayer.loadVideo(VIDEO_ID)
-//            }
-//
-//            override fun onInitializationFailure(p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?) {
-//                System.out.print("hi")
-//            }
-//
-//        }
-//
-//        play.setOnClickListener {
-//            youtubePlayer.initialize(YOUTUBE_API_KEY, youtubePlayerInit)
-//        }
-//
-
-
+        var view: View = inflater.inflate(R.layout.fragment_videos, container, false)
+        this.go = view.findViewById(R.id.go_Button) as Button
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        this.youtubePlayerInit = object : YouTubePlayer.OnInitializedListener {
-            override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, youtubePlayer: YouTubePlayer, p2: Boolean) {
-                youtubePlayer.loadVideo(VIDEO_ID)
-            }
-
-            override fun onInitializationFailure(p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?) {
-                System.out.print("hi")
-            }
-
-        }
-
-        play.setOnClickListener {
-            youtubePlayer.initialize(YOUTUBE_API_KEY, youtubePlayerInit)
+        go_Button.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=ckOJChcFP2g"))
+            startActivity(i)
         }
     }
-
-//    private fun initUI() {
-//        this.youtubePlayerInit = object : YouTubePlayer.OnInitializedListener {
-//            override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, youtubePlayer: YouTubePlayer, p2: Boolean) {
-//                youtubePlayer.loadVideo(VIDEO_ID)
-//            }
-//
-//            override fun onInitializationFailure(p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?) {
-//                System.out.print("hi")
-//            }
-//
-//        }
-//
-//        play.setOnClickListener(View.OnClickListener { youtubePlayer.initialize(YOUTUBE_API_KEY, youtubePlayerInit) })
-////        play.setOnClickListener(View.OnClickListener { v -> youtubePlayer.initialize(YOUTUBE_API_KEY, youtubePlayerInit) })
-//    }
-
 
 }
