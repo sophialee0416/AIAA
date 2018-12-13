ceb7f09b1417a639623475d87683255ef1728698
root <root@DESKTOP-QOC1HKH.localdomain>
2018-12-12 23:48:12 -0600

    i'm getting panic

--- appAIAA/app/src/main/java/comsophialee0416aiaa/httpsgithub/appaiaa/VideosFragment.kt
+++ appAIAA/app/src/main/java/comsophialee0416aiaa/httpsgithub/appaiaa/VideosFragment.kt
@@ -10,18 +10,20 @@ import android.widget.Toast
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
 import com.google.android.youtube.player.YouTubePlayerView
+import com.google.android.youtube.player.YouTubeStandalonePlayer
 import kotlinx.android.synthetic.main.fragment_videos.*
 
 //1
 class VideosFragment : Fragment() {
 
     lateinit var play: Button
+    lateinit var youtubePlayer: YouTubePlayerView
     lateinit var youtubePlayerInit: YouTubePlayer.OnInitializedListener
 
     //2
     companion object {
-//        val VIDEO_ID: String = "ckOJChcFP2g"
-//        val YOUTUBE_API_KEY: String = "AIzaSyC1umsPU8msFm4U4ZBmxhqgY-7Q53VSFao"
+        val VIDEO_ID: String = "ckOJChcFP2g"
+        val YOUTUBE_API_KEY: String = "AIzaSyC1umsPU8msFm4U4ZBmxhqgY-7Q53VSFao"
 
         fun newInstance(): VideosFragment {
             return VideosFragment()
@@ -37,10 +39,9 @@ class VideosFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        val view: View = inflater.inflate(R.layout.fragment_videos, container, false)
-//        this.play = view.findViewById(R.id.playButton) as Button
-
-
+        val view: View = inflater?.inflate(R.layout.fragment_videos, container, false)
+        this.play = view.findViewById(R.id.playButton) as Button
+        this.youtubePlayer = view.findViewById(R.id.youtubePlayerView) as YouTubePlayerView
 
 //        this.youtubePlayerInit = object : YouTubePlayer.OnInitializedListener {
 //            override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, youtubePlayer: YouTubePlayer, p2: Boolean) {
@@ -62,24 +63,24 @@ class VideosFragment : Fragment() {
         return view
     }
 
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//        this.youtubePlayerInit = object : YouTubePlayer.OnInitializedListener {
-//            override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, youtubePlayer: YouTubePlayer, p2: Boolean) {
-//                youtubePlayer.loadVideo(VIDEO_ID)
-//            }
-//
-//            override fun onInitializationFailure(p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?) {
-//                System.out.print("hi")
-//            }
-//
-//        }
-//
-//        play.setOnClickListener {
-//            youtubePlayer.initialize(YOUTUBE_API_KEY, youtubePlayerInit)
-//        }
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        this.youtubePlayerInit = object : YouTubePlayer.OnInitializedListener {
+            override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, youtubePlayer: YouTubePlayer, p2: Boolean) {
+                youtubePlayer.loadVideo(VIDEO_ID)
+            }
+
+            override fun onInitializationFailure(p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?) {
+                System.out.print("hi")
+            }
+
+        }
+
+        play.setOnClickListener {
+            youtubePlayer.initialize(YOUTUBE_API_KEY, youtubePlayerInit)
+        }
+    }
 
 //    private fun initUI() {
 //        this.youtubePlayerInit = object : YouTubePlayer.OnInitializedListener {
