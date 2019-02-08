16a4c53c7531a68f114433cd564f65144ccc017c
root <root@DESKTOP-QOC1HKH.localdomain>
2019-02-08 03:20:42 -0600

    reverting video to working state

--- appAIAA/app/src/main/java/comsophialee0416aiaa/httpsgithub/appaiaa/VideosFragment.kt
+++ appAIAA/app/src/main/java/comsophialee0416aiaa/httpsgithub/appaiaa/VideosFragment.kt
@@ -8,11 +8,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import com.google.android.youtube.player.YouTubePlayerSupportFragment
 import kotlinx.android.synthetic.main.fragment_videos.*
 
 //1
-class VideosFragment : YouTubePlayerSupportFragment() {
+class VideosFragment : Fragment() {
 
     lateinit var go: Button
 
