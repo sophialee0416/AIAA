9a1d27b2d5f067616b5f75fafec6ea6c728ad3aa
root <root@DESKTOP-QOC1HKH.localdomain>
2019-04-02 23:16:57 -0500

    Organized layout files

--- appAIAA/app/src/main/java/comsophialee0416aiaa/httpsgithub/appaiaa/CalculatorFragment.kt
+++ appAIAA/app/src/main/java/comsophialee0416aiaa/httpsgithub/appaiaa/CalculatorFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import kotlinx.android.synthetic.main.fragment_calculator.*
 
 // cards of formulas, same with videos
 
